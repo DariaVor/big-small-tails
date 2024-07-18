6926553d82db919adebedeb8a09b4215f75e3507
DariaVor <daria_voronina@list.ru>
2024-07-18 12:36:02 +0300

    update buttons

--- client/src/components/ui/NavBar.tsx
+++ client/src/components/ui/NavBar.tsx
@@ -31,12 +31,12 @@ export default function NavBar(): JSX.Element {
           <Link to="/">
             <img src="/images/doglogo.png" alt="Logo" style={{ width: '70px' }}/>
           </Link>
-          <Link to="/" className="text-2xl font-medium text-violet-900 font-rubik hover:text-violet-800 text-2xl">Хвосты и хвостики</Link>
+          <Link to="/" className="text-xl md:text-2xl font-medium text-violet-900 font-rubik hover:text-violet-800">Хвосты и хвостики</Link>
         </div>
         <button
           type='button'
           onClick={() => setIsMenuOpen(!isMenuOpen)}
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="navbar-default"
           aria-expanded={isMenuOpen}
         >
@@ -47,14 +47,14 @@ export default function NavBar(): JSX.Element {
                   className="pt-0.5"
                 />
         </button>
-        <div className={`w-full md:block md:w-auto ${isMenuOpen ? 'block' : 'hidden'}`} id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <div className={`w-full lg:block lg:w-auto ${isMenuOpen ? 'block' : 'hidden'}`} id="navbar-default">
+          <ul className="font-medium flex flex-col p-4 lg:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 lg:flex-row lg:space-x-8 lg:mt-0 lg:border-0 lg:bg-white dark:bg-gray-800 lg:dark:bg-gray-900 dark:border-gray-700">
             {navLinks.map((link) =>
               (!link.private || (link.private && user.status === 'logged')) && (
                 <li key={link.path}>
                   <Link
                     to={link.path}
-                    className="text-violet-900 hover:text-violet-700 font-rubik font-regular text-lg block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-violet-800 md:p-0 dark:text-white md:dark:hover:text-violet-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                    className="text-violet-900 hover:text-violet-700 font-rubik font-regular text-lg block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-violet-800 lg:p-0 dark:text-white lg:dark:hover:text-violet-500 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent"
                   >
                     {link.label}
                   </Link>
