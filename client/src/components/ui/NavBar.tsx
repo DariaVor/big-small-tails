3a95181b51f44ffb403069a63a48828d3afcc84a
DariaVor <daria_voronina@list.ru>
2024-07-19 08:40:25 +0300

    update dark

--- client/src/components/ui/NavBar.tsx
+++ client/src/components/ui/NavBar.tsx
@@ -25,7 +25,7 @@ export default function NavBar(): JSX.Element {
   ];
 
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900 shadow-md">
+    <nav className="bg-white border-gray-200 shadow-md">
       <div className="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto p-4">
         <div className="flex items-center space-x-4">
           <Link to="/">
@@ -36,7 +36,7 @@ export default function NavBar(): JSX.Element {
         <button
           type='button'
           onClick={() => setIsMenuOpen(!isMenuOpen)}
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
           aria-controls="navbar-default"
           aria-expanded={isMenuOpen}
         >
@@ -48,13 +48,13 @@ export default function NavBar(): JSX.Element {
                 />
         </button>
         <div className={`w-full md:block md:w-auto ${isMenuOpen ? 'block' : 'hidden'}`} id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
             {navLinks.map((link) =>
               (!link.private || (link.private && user.status === 'logged')) && (
                 <li key={link.path}>
                   <Link
                     to={link.path}
-                    className="text-violet-900 hover:text-violet-700 font-rubik font-regular text-lg block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-violet-800 md:p-0 dark:text-white md:dark:hover:text-violet-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                    className="text-violet-900 hover:text-violet-700 font-rubik font-regular text-lg block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-violet-800 md:p-0"
                   >
                     {link.label}
                   </Link>
@@ -72,7 +72,7 @@ export default function NavBar(): JSX.Element {
               ) : (
                 <Link
                   to="/login"
-                  className="text-violet-900 hover:text-violet-700 font-rubik font-regular text-lg block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-violet-800 md:p-0 dark:text-white md:dark:hover:text-violet-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="text-violet-900 hover:text-violet-700 font-rubik font-regular text-lg block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-violet-800 md:p-0"
                 >
                   Войти
                 </Link>
